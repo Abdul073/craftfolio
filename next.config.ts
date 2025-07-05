f3e98ddeba2ade379c85648426640f9958420b35
Abdul gaffar <abdulgaffar7274@gmail.com>
2025-07-05 17:38:51 +0530

    origin

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["http://52.90.1.49:3000"],
 };
 
 export default nextConfig;
